6dc8dd8ec8a6486a5eb400e95a9e1e416f6f3d43
Eric Thomas D. Cabigting <thomas@ericcabigting.dev>
2025-04-16 12:26:05 +0400

    update
    - added new image logo assets
    - update the home page
    - added images.d.ts to shutup my lsp from complaining the image is not a
    module when importing one

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Logo from '../assets/img/logo_light.png'
+
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={Logo} style={styles.img} />
+      <Text style={[styles.title, { fontSize: 50 }]}>Read List</Text>
+      <Text style={{ marginTop: 10, marginBottom: 13 }}>Reading list app</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  img: {
+    marginVertical: 50
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+})
